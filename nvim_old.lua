628812cdbd584f8677d5dfc81ed7e2178123182e
Filip Bengtegård <1380257+Insanityandme@users.noreply.github.com>
2024-11-07 11:20:51 +0100

    add lspconfig and treesitter and lazy.nvim to my config

--- nvim_old.lua
+++ nvim_old.lua
@@ -1,7 +1,178 @@
--- DEBUG: echo exepath('server_executable')
+-- TODO: Learn about treesitter and see if it's helpful
+-- TODO: Learn more about commands in nvims LSPs
+-- TODO: get autocompletion to work
+-- DEBUG: echo exepath('server_executable'
 -- Cool shortcuts: >i} (indent inside of {}) or any symbol.
 
+-- LSP config for C/C++
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+    pattern = {"*.c", "*.h", "*.cpp"},
+    callback = function()
+        vim.lsp.start({
+            name = 'c-lsp',
+            cmd = {'clangd'},
+        })
+    end
+})
+
+-- LSP config for Python 
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+    pattern = {"*.py"},
+    callback = function()
+        vim.lsp.start({
+            name = 'python-lsp',
+            cmd = {'jedi-language-server'},
+        })
+    end
+})
+
+-- LSP config for Javacript/Typescript 
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+    pattern = {"*.js", "*.ts", "*.tsx"},
+    callback = function()
+        vim.lsp.start({
+            name = 'js-lsp',
+            cmd = {'typescript-language-server.cmd', '--stdio'}
+        })
+    end
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.hoverProvider then
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+    end
+  end,
+})
+
+-- Toggle LSP diagnostics
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "toggle diagnostic" })
+
+
+-- Go to definition (in a split)
+function definition_split()
+  vim.lsp.buf.definition({
+    on_list = function(options)
+      -- if there are multiple items, warn the user
+      if #options.items > 1 then
+        vim.notify("Multiple items found, opening first one", vim.log.levels.WARN)
+      end
+
+      -- Open the first item in a vertical split
+      local item = options.items[1]
+      local cmd = "vsplit +" .. item.lnum .. " " .. item.filename .. "|" .. "normal " .. item.col .. "|"
+
+      vim.cmd("wincmd o")
+      vim.cmd(cmd)
+      vim.cmd("wincmd p") 
+      vim.cmd("wincmd r")
+    end,
+  })
+end
+
+-- Go to declaration (in a split)
+function declaration_split()
+  vim.lsp.buf.declaration({
+    on_list = function(options)
+      -- if there are multiple items, warn the user
+      if #options.items > 1 then
+        vim.notify("Multiple items found, opening first one", vim.log.levels.WARN)
+      end
+
+      -- Open the first item in a vertical split
+      local item = options.items[1]
+      local cmd = "vsplit +" .. item.lnum .. " " .. item.filename .. "|" .. "normal " .. item.col .. "|" 
+
+      vim.cmd("wincmd o")
+      vim.cmd(cmd)
+      vim.cmd("wincmd p") 
+      vim.cmd("wincmd r")
+    end,
+  })
+end
+
+-- toggles C LSP on and off
+function toggle_lsp() 
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+        vim.lsp.start({
+            name = 'c-lsp',
+            cmd = {'clangd'},
+        })
+    else 
+        vim.lsp.stop_client(vim.lsp.get_active_clients())
+    end 
+end
+
+-- Removes highlighting from LSP
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = true
+  end,
+});
+
+-- AUTO COMPLETION - COURTESY OF mattb on stackoverflow
+-- vim.cmd([[
+--     set completeopt+=menuone,noselect,noinsert " don't insert text automatically
+-- 
+--     set pumheight=5 " keep the autocomplete suggestion menu small
+--     set shortmess+=c " don't give ins-completion-menu messages
+-- 
+--     " if completion menu closed, and two non-spaces typed, call autocomplete
+--     let s:insert_count = -1
+--     function! OpenCompletion()
+--         if string(v:char) =~ ' '
+--             let s:insert_count = 0
+--         else                    
+--             let s:insert_count += 1
+--         endif
+--         if !pumvisible() && s:insert_count >= 2
+--             silent! call feedkeys("\<C-n>", "n")
+--         endif
+--     endfunction
+-- 
+--     function! TurnOnAutoComplete()
+--     augroup autocomplete
+--         autocmd!
+--         autocmd InsertLeave let s:insert_count = 0
+--         autocmd InsertCharPre * silent! call OpenCompletion()
+--     augroup END
+--     endfunction
+-- 
+--     function! TurnOffAutoComplete()
+--     augroup autocomplete
+--         autocmd!
+--     augroup END
+--     endfunction
+-- 
+--     function! ReplayMacroWithoutAutoComplete()
+--     call TurnOffAutoComplete()
+--     let reg = getcharstr()
+--     execute "normal! @".reg
+--     call TurnOnAutoComplete()
+--     endfunction
+-- 
+--     call TurnOnAutoComplete()
+-- 
+--     " don't let the above mess with replaying macros
+--     nnoremap <silent> @ :call ReplayMacroWithoutAutoComplete()<CR>
+-- 
+-- 
+-- ]])
+
+-- MY OWN STUFF
 vim.cmd([[
+
     set number
     set relativenumber
     let mapleader = " "
@@ -13,10 +184,7 @@ vim.cmd([[
     set shiftwidth=4 " Indents will have a width of 4.
     set expandtab " Expand TABS to spaces.
     set scrolloff=8
-    " set nowrap
-    " set textwidth=80
-    set foldmethod=marker
-    set ff=unix
+    set nowrap
 
     let g:gruvbox_contrast_dark = 'hard'
     colorscheme gruvbox
@@ -44,11 +212,11 @@ vim.cmd([[
     " and if not, open the current working directory in Netrw.
     augroup InitNetrw
       autocmd!
-      autocmd VimEnter * if expand("%") == "" | edit . | endif
+      autocmd VimEnter * if expand("%") == "" | edit . | vsplit | endif
     augroup END
 
     " Ensure the buffer for building code opens in a new view, does it?
-    " set switchbuf=uselast,split
+    set switchbuf=uselast,split
 
     " Thanks to https://forums.handmadehero.org/index.php/forum?view=topic&catid=4&id=704#3982
     " error message formats
@@ -125,10 +293,10 @@ vim.cmd([[
     " nnoremap <M-m> :call BuildProject()<CR>
 
     " execute current python file
-    " nnoremap <M-m> :call ExecutePython()<CR>
+    nnoremap <M-m> :call ExecutePython()<CR>
 
     " execute test python file
-    " nnoremap <M-p> :call ExecutePythonTestShell()<CR>
+    nnoremap <M-p> :call ExecutePythonTestShell()<CR>
 
     " autocmd! FileType qf nnoremap <buffer> <leader><Enter> <C-w><Enter><C-w>L
 
@@ -167,6 +335,7 @@ vim.cmd([[
    
     " to map <Esc> to exit terminal-mode
     tnoremap <Esc> <C-\><C-n>
+
 ]])
 
 -- Insert Caseys source format
@@ -204,19 +373,18 @@ vim.api.nvim_create_autocmd({"BufNewFile"}, {
         '#endif'
 })
 
--- Automatically run python tests when saving a .py file
--- vim.api.nvim_create_autocmd({"BufWritePost"}, {
---     pattern = {"*.py"},
---     command = ':call ExecutePythonTestShell()'
--- })
+vim.api.nvim_create_autocmd({"BufWritePost"}, {
+    pattern = {"*.py"},
+    command = ':call ExecutePythonTestShell()'
+})
 
 -- Beautiful highlights =D
 -- IF contrast is medium, fg = #282828
 -- IF contrast is hard, fg = #1d2021
-vim.api.nvim_set_hl(0, "NOTE", {underline=true,reverse=true,fg="#1d2021",bg="Green" })
-vim.api.nvim_set_hl(0, "TODO", {underline=true,reverse=true,fg="#1d2021",bg="Red"})
-vim.api.nvim_set_hl(0, "IMPORTANT", {underline=true,reverse=true,fg="#1d2021",bg="Yellow"})
-vim.api.nvim_set_hl(0, "STUDY", {underline=true,reverse=true,fg="#1d2021",bg="Yellow"})
+vim.api.nvim_set_hl(0, "Note", {underline=true,reverse=true,fg="#1d2021",bg="Green" })
+vim.api.nvim_set_hl(0, "Todo", {underline=true,reverse=true,fg="#1d2021",bg="Red"})
+vim.api.nvim_set_hl(0, "Important", {underline=true,reverse=true,fg="#1d2021",bg="Yellow"})
+vim.api.nvim_set_hl(0, "Study", {underline=true,reverse=true,fg="#1d2021",bg="Yellow"})
 
 -- Need to execute matchadd for the new highlight group Note
 vim.api.nvim_create_autocmd("VimEnter", {
@@ -226,203 +394,6 @@ vim.api.nvim_create_autocmd("VimEnter", {
         vim.fn.execute("execute matchadd(\"Study\", \"STUDY\")")
     end
 })
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Setup lazy.nvim
-require("lazy").setup({
-    {
-    -- add your plugins here
-    'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',  -- Automatically update parsers when needed
-        config = function()
-          require'nvim-treesitter.configs'.setup {
-            -- Install the languages you need, or use "maintained" for all maintained parsers
-            ensure_installed = {"typescript", "c", "hcl"}, -- Adjust to the languages you need
-            highlight = {
-              enable = true,  -- Enable syntax highlighting using Treesitter
-              additional_vim_regex_highlighting = false,  -- Disable Vim regex-based highlighting
-            },
-            incremental_selection = {
-              enable = true,
-              keymaps = {
-                init_selection = "<CR>",  -- Start a selection
-                node_incremental = "<Tab>",  -- Select the next node
-                node_decremental = "<S-Tab>",  -- Select the previous node
-              },
-            },
-            indent = {
-              enable = true,  -- Enable Treesitter-based indentation
-            },
-          }
-      end
-      },
-     -- Install nvim-lspconfig for LSP support
-      {
-        'neovim/nvim-lspconfig',
-        config = function()
-          local lspconfig = require('lspconfig')
-          local util = require('lspconfig.util')
-
-          -- Configure Deno LSP
-          lspconfig.denols.setup{
-            root_dir = util.root_pattern('deno.json', 'deno.jsonc'),
-            settings = {
-              deno = {
-                enable = true,
-                unstable = true,  -- Optional: Enable unstable APIs if needed
-              },
-            },
-          }
-
-          -- Configure TypeScript LSP (tsserver)
-          lspconfig.ts_ls.setup{
-            root_dir = util.root_pattern('tsconfig.json', 'package.json', '.git'),
-            on_attach = function(client, bufnr)
-              -- Disable tsserver in Deno projects
-              if util.root_pattern('deno.json', 'deno.jsonc')(vim.fn.bufname(bufnr)) then
-                client.stop()
-              end
-            end,
-          }
-        end
-      },
-})
-
--- LSP config for C/C++
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-    pattern = {"*.c", "*.h", "*.cpp"},
-    callback = function()
-        vim.lsp.start({
-            name = 'c-lsp',
-            cmd = {'clangd'},
-        })
-    end
-})
-
--- LSP config for Python 
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-    pattern = {"*.py"},
-    callback = function()
-        vim.lsp.start({
-            name = 'python-lsp',
-            cmd = {'jedi-language-server'},
-        })
-    end
-})
-
--- LSP config for Javacript/Typescript 
--- vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
---     pattern = {"*.js", "*.ts", "*.tsx"},
---     callback = function()
---         vim.lsp.start({
---             name = 'js-lsp',
---             cmd = {'typescript-language-server.cmd', '--stdio'}
---         })
---     end
--- })
-
--- Set filetype to hcl for .tf files
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-    pattern = {"*.tf", "*.tfvars", "*.tfstack.hcl", "*.tfdeploy.hcl"},
-    callback = function()
-        vim.bo.filetype = "hcl"
-    end
-})
--- LSP config for Terraform 
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-    pattern = {"*.tf", "*.tfvars", "*.tfstack.hcl", "*.tfdeploy.hcl"},
-    callback = function()
-        vim.lsp.start({
-            name = 'terraform-ls',
-            cmd = {'terraform-ls', 'serve'}
-        })
-    end
-})
-
--- Toggle LSP diagnostics
-vim.api.nvim_create_user_command("DiagnosticToggle", function()
-	local config = vim.diagnostic.config
-	local vt = config().virtual_text
-	config {
-		virtual_text = not vt,
-		underline = not vt,
-		signs = not vt,
-	}
-end, { desc = "toggle diagnostic" })
-
--- Hover
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client.server_capabilities.hoverProvider then
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
-    end
-  end,
-})
-
--- Go to definition (in a split)
-function definition_split()
-  vim.lsp.buf.definition({
-    on_list = function(options)
-      -- if there are multiple items, warn the user
-      if #options.items > 1 then
-        vim.notify("Multiple items found, opening first one", vim.log.levels.WARN)
-      end
-
-      -- Open the first item in a vertical split
-      local item = options.items[1]
-      local cmd = "vsplit +" .. item.lnum .. " " .. item.filename .. "|" .. "normal " .. item.col .. "|"
-
-      vim.cmd("wincmd o")
-      vim.cmd(cmd)
-      vim.cmd("wincmd p") 
-      vim.cmd("wincmd r")
-    end,
-  })
-end
-
--- toggles C LSP on and off
-function toggle_lsp() 
-    local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then
-        vim.lsp.start({
-            name = 'c-lsp',
-            cmd = {'clangd'},
-        })
-    else 
-        vim.lsp.stop_client(vim.lsp.get_active_clients())
-    end 
-end
-
--- Removes highlighting from LSP
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    client.server_capabilities.semanticTokensProvider = true
-  end,
-});
 
 -- Uses omnifunc for autocompletion
 vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -439,5 +410,3 @@ vim.keymap.set("n", '<leader>t', function() vim.cmd.terminal() end)
 -- https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 vim.cmd([[autocmd FileType * set formatoptions-=ro]])
-
-
